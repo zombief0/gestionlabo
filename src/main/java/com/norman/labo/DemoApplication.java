315ab2d99d2f2534e909d407531be0ab308d0d4a
Norman <mbouendenorman@gmail.com>
2025-04-24 19:27:17 +0200

    change from war to jar

--- src/main/java/com/norman/labo/DemoApplication.java
+++ src/main/java/com/norman/labo/DemoApplication.java
@@ -2,19 +2,12 @@ package com.norman.labo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DemoApplication extends SpringBootServletInitializer {
+public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
-    }
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder){
-        return builder.sources(DemoApplication.class);
     }
 
 }
